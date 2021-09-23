9e0358148da514758ca42c029fc0f0bf8a707ad9
bespokeeagle <49434547+bespokeeagle@users.noreply.github.com>
2021-09-23 17:26:49 +0100

    Add files via upload

---
+++ ubuntu/variable.tf
@@ -0,0 +1,13 @@
+
+variable "name" {
+    type = string
+    default = "test"
+
+}
+
+variable "key"{
+    type = string
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsglPvFXuz3LTd+fpRxLd+mnPmHB3/v+2NjrmgqhBX+AhZDRPD2zAic/evAQyNzmegOzFn5WXGlLAidmFwARCQG1C6ZQIoKm8OLIFq0cXhvARa2sZJeMk7BL3vfQXnAEB7Kc32+E6jTSZkfH9SpOo97RW3WdittSPWpmgYWeM1oEUPCDqbHGSMuTeN2wkOJHM+ILZkK6c/T3NXITlgSh6nusJMWodNhVAVJwzRNHDNHNde/p+B4DxkMdwdF4QgoqYQym7agYMUDxux5hKG1yHtfVhe+xwkrxhdxCZilDbc4X9LXJd1xPkT3ptVsd+tn1FjmRVwCF7skvx0A2CEt8M9"
+
+
+}
